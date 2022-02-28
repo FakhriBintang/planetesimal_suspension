ee1d343fd918a5dab48580e2e6d5a06a03552664
Tobias Keller <kellertobs@gmail.com>
2022-02-28 16:57:31 +0100

    add 0D plotting functionality

--- Fe_melting/src/output.m
+++ Fe_melting/src/output.m
@@ -11,248 +11,298 @@ if RUN.plot
     TX = {'Interpreter','Latex'}; FS = {'FontSize',12};
     TL = {'TickLabelInterpreter','Latex'}; TS = {'FontSize',10};
     UN = {'Units','Centimeters'};
-    xq = round(NUM.N/10/2)+1:round(NUM.N/10):NUM.nxP;  % x-indexes for quiver plots
-    zq = round(NUM.N/10/2)+1:round(NUM.N/10):NUM.nxP;  % z-indexes for quiver plots
     
+    if NUM.Nx <= 10 && NUM.Nz <= 10  % create 0D plots
+        
+        if iter > 0
+            
+            fh1 = figure(1); clf;
+            subplot(4,1,1)
+            ph1 = plot(HST.time/NUM.yr,HST.T(:,2),'LineWidth',2); axis xy tight; box on;
+            title('$T [^\circ$C]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(4,1,2)
+            plot(HST.time/NUM.yr,HST.xFe(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title('$x_{Fe}$ [wt\% Fe-FeS]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(4,1,3)
+            plot(HST.time/NUM.yr,HST.cFe(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title('$\bar{c}_{Fe}$ [wt\% S]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(4,1,4)
+            plot(HST.time/NUM.yr,HST.cSi(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title('$\bar{c}_{Si}$ [wt\% SiO$_2$]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            xlabel('Time [yr]',TX{:},FS{:});
 
-    %% plot velocity and temperature solutions
-    fh1  = figure(1); clf
-    figure(1)
-    subplot(2,3,1)
-    imagesc(NUM.xU,NUM.zU,SOL.U); hold on;
-    quiver(NUM.xP(xq),NUM.zP(zq),SOL.UP(zq,xq),SOL.WP(zq,xq),'k')
-    colormap(subplot(2,3,1),cm2)
-    axis ij equal tight;
-    colorbar
-    title('v^*_x [ms^-^1]')
-    
-    subplot(2,3,2)
-    imagesc(NUM.xW,NUM.zW,-SOL.W); hold on;
-    quiver(NUM.xP(xq),NUM.zP(zq),SOL.UP(zq,xq),SOL.WP(zq,xq),'k')
-    colormap(subplot(2,3,2),cm2)
-    axis ij equal tight;
-    colorbar
-    title('v_z^*-velocity [ms^-^1]')
-
-    subplot(2,3,3)
-    imagesc(NUM.xP,NUM.zP,SOL.P); hold on;
-    colormap(subplot(2,3,3),cm2)
-    axis ij equal tight;
-    colorbar
-    title('dynamic pressure [Pa]')
-    
-    Tplot = SOL.T;
-    subplot(2,3,4)
-    imagesc(NUM.xP,NUM.zP,SOL.T);
-    colormap(subplot(2,3,4),flipud(cm1))
-    axis ij equal tight;
-    colorbar
-    title('Temperature [C]')
-
-    subplot(2,3,5);
-    imagesc(NUM.xP,NUM.zP,MAT.rho);
-    colormap(subplot(2,3,5),cm1)
-    axis ij equal tight;
-    colorbar
-    title('bulk density [kgm^-^3]')
-    
-    subplot(2,3,6);
-    imagesc(NUM.xP,NUM.zP,log10(MAT.Eta));
-    colormap(subplot(2,3,6),cm1)
-    axis ij equal tight;
-    colorbar
-    title('\eta [log_1_0 Pas]')
-
-    
-    %% plot phase vol fractions
-    fh2 = figure(2); clf;
-    figure(2) % plot phase fractions
-    subplot(2,2,1);
-    imagesc(NUM.xP,NUM.zP,SOL.phiSil);
-    colorbar
-    axis ij equal tight;
-    title('\phi_{Si}^l')
-    subplot(2,2,2);
-    imagesc(NUM.xP,NUM.zP,SOL.phiSis);
-    colorbar
-    axis ij equal tight;
-    title('\phi_{Si}^s')
-    subplot(2,2,3);
-    imagesc(NUM.xP,NUM.zP,SOL.phiFel);
-    colorbar
-    axis ij equal tight;
-    title('\phi_{Fe}^l')
-    subplot(2,2,4);
-    imagesc(NUM.xP,NUM.zP,SOL.phiFes);
-    colorbar
-    axis ij equal tight;
-    title('\phi_{Fe}^s')
-    
-    
-    %% plot phase
-    fh3 = figure(3); clf
-    figure(3) % plot phase fractions
-    subplot(2,2,1);
-    imagesc(NUM.xP,NUM.zP,CHM.fSil);
-    colorbar
-    axis ij equal tight  
-    title('f_{Si}^l [wt]')
-    subplot(2,2,2);
-    imagesc(NUM.xP,NUM.zP,CHM.fSis);
-    colorbar
-    axis ij equal tight  
-    title('f_{Si}^s [wt]')
-    subplot(2,2,3);
-    imagesc(NUM.xP,NUM.zP,CHM.fFel);
-    colorbar
-    axis ij equal tight  
-    title('f_{Fe}^l [wt]')
-    subplot(2,2,4);
-    imagesc(NUM.xP,NUM.zP,CHM.fFes);
-    colorbar
-    axis ij equal tight  
-    title('f_{Fe}^s [wt]')
-    
-    
-    %% plot phase segregation
-    fh4 = figure(4); clf
-    figure(4) % plot phase segregation
-    subplot(2,2,1);
-    imagesc(NUM.xW,NUM.zW,SOL.phiFes+SOL.phiFel+SOL.phiSis+SOL.phiSil);
-    colorbar
-    axis ij equal tight  
-    title('total phi')
-    
-    subplot(2,2,2);
-    imagesc(NUM.xP,NUM.zP,-segSis);
-    colorbar
-    axis ij equal tight  
-    title('v_{\Delta, Si}^s')
-    
-    subplot(2,2,3);
-    imagesc(NUM.xP,NUM.zP,-segFel);
-    colorbar
-    axis ij equal tight  
-    title('v_{\Delta, Fe}^l')
-    
-    subplot(2,2,4);
-    imagesc(NUM.xP,NUM.zP,-segFes);
-    colorbar
-    axis ij equal tight  
-    title('v_{\Delta, Fe}^s')
-    
-    
-    %% plot system fractions and chemical solutions
-    fh5 = figure(5); clf
-    figure(5)
-    subplot(2,2,1)
-    imagesc(NUM.xP,NUM.zP,CHM.xFe)
-    colorbar
-    axis ij equal tight  
-    title('x_{Fe} [wt]')
-    
-    subplot(2,2,2)
-    imagesc(NUM.xP,NUM.zP,CHM.xSi)
-    colorbar
-    axis ij equal tight  
-    title('x_{Si} [wt]')
-    
-    subplot(2,2,3)
-    imagesc(NUM.xP,NUM.zP,CHM.cFe)
-    colorbar
-    axis ij equal tight  
-    title('c_{Fe} [wt]')
-    
-    subplot(2,2,4)
-    imagesc(NUM.xP,NUM.zP,CHM.cSi)
-    colorbar
-    axis ij equal tight  
-    title('c_{Si} [wt]')
-    
-    
-    %% plot phase densities
-    fh6 = figure(6); clf
-    figure(6)
-    subplot(2,2,1)
-    imagesc(NUM.xP,NUM.zP,MAT.rhoSil)
-    colorbar
-    axis ij equal tight  
-    title('\rho_{Si}^l')
-    
-    subplot(2,2,2)
-    imagesc(NUM.xP,NUM.zP,MAT.rhoSis)
-    colorbar
-    axis ij equal tight  
-    title('\rho_{Si}^s')
-    
-    subplot(2,2,3)
-    imagesc(NUM.xP,NUM.zP,MAT.rhoFel)
-    colorbar
-    axis ij equal tight  
-    title('\rho_{Fe}^l')
-    
-    subplot(2,2,4)
-    imagesc(NUM.xP,NUM.zP,MAT.rhoFes)
-    colorbar
-    axis ij equal tight  
-    title('\rho_{Fe}^s')
-    
-    
-    %% plot chemical phase compositions
-    fh7 = figure(7); clf
-    figure(7)
-    subplot(2,2,1)
-    imagesc(NUM.xP,NUM.zP,CHM.clSi)
-    colorbar
-    axis ij equal tight  
-    title('c_{Si}^l')
-    
-    subplot(2,2,2)
-    imagesc(NUM.xP,NUM.zP,CHM.csSi)
-    colorbar
-    axis ij equal tight  
-    title('c_{Si}^s')
-    
-    subplot(2,2,3)
-    imagesc(NUM.xP,NUM.zP,CHM.clFe)
-    colorbar
-    axis ij equal tight  
-    title('c_{Fe}^l')
-    
-    subplot(2,2,4)
-    imagesc(NUM.xP,NUM.zP,CHM.csFe)
-    colorbar
-    axis ij equal tight  
-    title('c_{Fe}^s')
-    
-    
-    %% plot Partial densities and
-    fh8 = figure(8); clf;
-    figure(8)
-    subplot(2,2,1)
-    imagesc(NUM.xP,NUM.zP,CHM.CFe)
-    colorbar
-    axis ij equal tight  
-    title('C_{Fe} ')
-    
-    subplot(2,2,2)
-    imagesc(NUM.xP,NUM.zP,CHM.CSi)
-    colorbar
-    axis ij equal tight  
-    title('C_{Si} ')
-    
-    subplot(2,2,3)
-    imagesc(NUM.xP,NUM.zP,CHM.XFe)
-    colorbar
-    axis ij equal tight  
-    title('X_{Fe}')
-    
-    subplot(2,2,4)
-    imagesc(NUM.xP,NUM.zP,CHM.XSi)
-    colorbar
-    axis ij equal tight  
-    title('X_{Si}')
-    
+            fh2 = figure(2); clf;
+            subplot(6,1,1)
+            plot(HST.time/NUM.yr,HST.fFel(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title(['$f_{Fe}$ [wt\%]'],TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(6,1,2)
+            plot(HST.time/NUM.yr,HST.fSil(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title(['$f_{Si}$ [wt\%]'],TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(6,1,3)
+            plot(HST.time/NUM.yr,HST.phiFel(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title('$\phi_{Fe}^s$ [vol\%]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(6,1,4)
+            plot(HST.time/NUM.yr,HST.phiSil(:,2).*100,'LineWidth',2); axis xy tight; box on;
+            title('$\phi_{Si}^\ell$ [vol\%]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(6,1,5)
+            plot(HST.time/NUM.yr,HST.rho(:,2),'LineWidth',2); axis xy tight; box on;
+            title('$\bar{\rho}$ [kg/m$^3$]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            subplot(6,1,6)
+            plot(HST.time/NUM.yr,log10(HST.eta(:,2)),'LineWidth',2); axis xy tight; box on;
+            title('$\bar{\eta}$ [log$_{10}$ Pas]',TX{:},FS{:}); set(gca,TL{:},TS{:});
+            xlabel('Time [hr]',TX{:},FS{:});
+            
+        end
+        
+    else
+        
+        % prepare for plotting
+        TX = {'Interpreter','Latex'}; FS = {'FontSize',12};
+        TL = {'TickLabelInterpreter','Latex'}; TS = {'FontSize',10};
+        UN = {'Units','Centimeters'};
+        xq = round(NUM.N/10/2)+1:round(NUM.N/10):NUM.nxP;  % x-indexes for quiver plots
+        zq = round(NUM.N/10/2)+1:round(NUM.N/10):NUM.nxP;  % z-indexes for quiver plots
+        
+        
+        %% plot velocity and temperature solutions
+        fh1  = figure(1); clf
+        figure(1)
+        subplot(2,3,1)
+        imagesc(NUM.xU,NUM.zU,SOL.U); hold on;
+        quiver(NUM.xP(xq),NUM.zP(zq),SOL.UP(zq,xq),SOL.WP(zq,xq),'k')
+        colormap(subplot(2,3,1),cm2)
+        axis ij equal tight;
+        colorbar
+        title('v^*_x [ms^-^1]')
+        
+        subplot(2,3,2)
+        imagesc(NUM.xW,NUM.zW,-SOL.W); hold on;
+        quiver(NUM.xP(xq),NUM.zP(zq),SOL.UP(zq,xq),SOL.WP(zq,xq),'k')
+        colormap(subplot(2,3,2),cm2)
+        axis ij equal tight;
+        colorbar
+        title('v_z^*-velocity [ms^-^1]')
+        
+        subplot(2,3,3)
+        imagesc(NUM.xP,NUM.zP,SOL.P); hold on;
+        colormap(subplot(2,3,3),cm2)
+        axis ij equal tight;
+        colorbar
+        title('dynamic pressure [Pa]')
+        
+        Tplot = SOL.T;
+        subplot(2,3,4)
+        imagesc(NUM.xP,NUM.zP,SOL.T);
+        colormap(subplot(2,3,4),flipud(cm1))
+        axis ij equal tight;
+        colorbar
+        title('Temperature [C]')
+        
+        subplot(2,3,5);
+        imagesc(NUM.xP,NUM.zP,MAT.rho);
+        colormap(subplot(2,3,5),cm1)
+        axis ij equal tight;
+        colorbar
+        title('bulk density [kgm^-^3]')
+        
+        subplot(2,3,6);
+        imagesc(NUM.xP,NUM.zP,log10(MAT.Eta));
+        colormap(subplot(2,3,6),cm1)
+        axis ij equal tight;
+        colorbar
+        title('\eta [log_1_0 Pas]')
+        
+        
+        %% plot phase vol fractions
+        fh2 = figure(2); clf;
+        figure(2) % plot phase fractions
+        subplot(2,2,1);
+        imagesc(NUM.xP,NUM.zP,SOL.phiSil);
+        colorbar
+        axis ij equal tight;
+        title('\phi_{Si}^l')
+        subplot(2,2,2);
+        imagesc(NUM.xP,NUM.zP,SOL.phiSis);
+        colorbar
+        axis ij equal tight;
+        title('\phi_{Si}^s')
+        subplot(2,2,3);
+        imagesc(NUM.xP,NUM.zP,SOL.phiFel);
+        colorbar
+        axis ij equal tight;
+        title('\phi_{Fe}^l')
+        subplot(2,2,4);
+        imagesc(NUM.xP,NUM.zP,SOL.phiFes);
+        colorbar
+        axis ij equal tight;
+        title('\phi_{Fe}^s')
+        
+        
+        %% plot phase
+        fh3 = figure(3); clf
+        figure(3) % plot phase fractions
+        subplot(2,2,1);
+        imagesc(NUM.xP,NUM.zP,CHM.fSil);
+        colorbar
+        axis ij equal tight
+        title('f_{Si}^l [wt]')
+        subplot(2,2,2);
+        imagesc(NUM.xP,NUM.zP,CHM.fSis);
+        colorbar
+        axis ij equal tight
+        title('f_{Si}^s [wt]')
+        subplot(2,2,3);
+        imagesc(NUM.xP,NUM.zP,CHM.fFel);
+        colorbar
+        axis ij equal tight
+        title('f_{Fe}^l [wt]')
+        subplot(2,2,4);
+        imagesc(NUM.xP,NUM.zP,CHM.fFes);
+        colorbar
+        axis ij equal tight
+        title('f_{Fe}^s [wt]')
+        
+        
+        %% plot phase segregation
+        fh4 = figure(4); clf
+        figure(4) % plot phase segregation
+        subplot(2,2,1);
+        imagesc(NUM.xW,NUM.zW,-segSil);
+        colorbar
+        axis ij equal tight
+        title('total phi')
+        
+        subplot(2,2,2);
+        imagesc(NUM.xP,NUM.zP,-segSis);
+        colorbar
+        axis ij equal tight
+        title('v_{\Delta, Si}^s')
+        
+        subplot(2,2,3);
+        imagesc(NUM.xP,NUM.zP,-segFel);
+        colorbar
+        axis ij equal tight
+        title('v_{\Delta, Fe}^l')
+        
+        subplot(2,2,4);
+        imagesc(NUM.xP,NUM.zP,-segFes);
+        colorbar
+        axis ij equal tight
+        title('v_{\Delta, Fe}^s')
+        
+        
+        %% plot system fractions and chemical solutions
+        fh5 = figure(5); clf
+        figure(5)
+        subplot(2,2,1)
+        imagesc(NUM.xP,NUM.zP,CHM.xFe)
+        colorbar
+        axis ij equal tight
+        title('x_{Fe} [wt]')
+        
+        subplot(2,2,2)
+        imagesc(NUM.xP,NUM.zP,CHM.xSi)
+        colorbar
+        axis ij equal tight
+        title('x_{Si} [wt]')
+        
+        subplot(2,2,3)
+        imagesc(NUM.xP,NUM.zP,CHM.cFe)
+        colorbar
+        axis ij equal tight
+        title('c_{Fe} [wt]')
+        
+        subplot(2,2,4)
+        imagesc(NUM.xP,NUM.zP,CHM.cSi)
+        colorbar
+        axis ij equal tight
+        title('c_{Si} [wt]')
+        
+        
+        %% plot phase densities
+        fh6 = figure(6); clf
+        figure(6)
+        subplot(2,2,1)
+        imagesc(NUM.xP,NUM.zP,MAT.rhoSil)
+        colorbar
+        axis ij equal tight
+        title('\rho_{Si}^l')
+        
+        subplot(2,2,2)
+        imagesc(NUM.xP,NUM.zP,MAT.rhoSis)
+        colorbar
+        axis ij equal tight
+        title('\rho_{Si}^s')
+        
+        subplot(2,2,3)
+        imagesc(NUM.xP,NUM.zP,MAT.rhoFel)
+        colorbar
+        axis ij equal tight
+        title('\rho_{Fe}^l')
+        
+        subplot(2,2,4)
+        imagesc(NUM.xP,NUM.zP,MAT.rhoFes)
+        colorbar
+        axis ij equal tight
+        title('\rho_{Fe}^s')
+        
+        
+        %% plot chemical phase compositions
+        fh7 = figure(7); clf
+        figure(7)
+        subplot(2,2,1)
+        imagesc(NUM.xP,NUM.zP,CHM.clSi)
+        colorbar
+        axis ij equal tight
+        title('c_{Si}^l')
+        
+        subplot(2,2,2)
+        imagesc(NUM.xP,NUM.zP,CHM.csSi)
+        colorbar
+        axis ij equal tight
+        title('c_{Si}^s')
+        
+        subplot(2,2,3)
+        imagesc(NUM.xP,NUM.zP,CHM.clFe)
+        colorbar
+        axis ij equal tight
+        title('c_{Fe}^l')
+        
+        subplot(2,2,4)
+        imagesc(NUM.xP,NUM.zP,CHM.csFe)
+        colorbar
+        axis ij equal tight
+        title('c_{Fe}^s')
+        
+        
+        %% plot Partial densities and
+        fh8 = figure(8); clf;
+        figure(8)
+        subplot(2,2,1)
+        imagesc(NUM.xP,NUM.zP,CHM.CFe)
+        colorbar
+        axis ij equal tight
+        title('C_{Fe} ')
+        
+        subplot(2,2,2)
+        imagesc(NUM.xP,NUM.zP,CHM.CSi)
+        colorbar
+        axis ij equal tight
+        title('C_{Si} ')
+        
+        subplot(2,2,3)
+        imagesc(NUM.xP,NUM.zP,CHM.XFe)
+        colorbar
+        axis ij equal tight
+        title('X_{Fe}')
+        
+        subplot(2,2,4)
+        imagesc(NUM.xP,NUM.zP,CHM.XSi)
+        colorbar
+        axis ij equal tight
+        title('X_{Si}')
+        
+    end
     
     %% plot phase diagrams
     fh9 = figure(9); clf;
@@ -279,25 +329,25 @@ if RUN.plot
     
     
     %% plot conserved quantities
-    fh10 = figure(10);
+    fh10 = figure(10); clf;
     % mass conservation
-    if NUM.step > 0
+    if iter > 0
         subplot(5,1,1)
-        plot(NUM.time./3600,CON.EM  (NUM.step+1),'ko','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
-        ylabel('consv. $Mass$',TX{:},FS{:});
-        xlabel('Time [hr]',TX{:},FS{:});
+        plot(HST.time./NUM.yr,HST.EM  ,'k-','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
+        ylabel('consv. $Mass$',TX{:},FS{:}); set(gca,TL{:},TS{:},'XTickLabel',[]);
         subplot(5,1,2)
-        plot(NUM.time./3600,CON.EH  (NUM.step+1),'ko','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
-        ylabel('consv. $H$',TX{:},FS{:}); set(gca,'XTickLabel',[]);
+        plot(HST.time./NUM.yr,HST.EH  ,'k-','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
+        ylabel('consv. $H$',TX{:},FS{:}); set(gca,TL{:},TS{:},'XTickLabel',[]);
         subplot(5,1,3)
-        plot(NUM.time./3600,CON.EXFe(NUM.step+1),'ko','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
-        ylabel('consv. $X_{Fe}$',TX{:},FS{:}); set(gca,'XTickLabel',[]);
+        plot(HST.time./NUM.yr,HST.EXFe,'k-','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
+        ylabel('consv. $X_{Fe}$',TX{:},FS{:}); set(gca,TL{:},TS{:},'XTickLabel',[]);
         subplot(5,1,4)
-        plot(NUM.time./3600,CON.ECFe(NUM.step+1),'ko','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
-        ylabel('consv. $C_{Fe}$',TX{:},FS{:}); set(gca,'XTickLabel',[]);
+        plot(HST.time./NUM.yr,HST.ECFe,'k-','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
+        ylabel('consv. $C_{Fe}$',TX{:},FS{:}); set(gca,TL{:},TS{:},'XTickLabel',[]);
         subplot(5,1,5)
-        plot(NUM.time./3600,CON.ECSi(NUM.step+1),'ko','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
-        ylabel('consv. $C_{Si}$',TX{:},FS{:}); set(gca,'XTickLabel',[]);
+        plot(HST.time./NUM.yr,HST.ECSi,'k-','MarkerSize',5,'LineWidth',2); hold on; axis tight; box on;
+        ylabel('consv. $C_{Si}$',TX{:},FS{:}); set(gca,TL{:},TS{:})
+        xlabel('Time [yr]',TX{:},FS{:});
     end
     
 end
