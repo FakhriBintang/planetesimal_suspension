deefc9b72bccc158c39b6899d72fdef190c5e4c0
FakhriBintang <fakhri.bintang8@gmail.com>
2022-09-06 12:07:24 +0100

    Chaged thermochemistry to the entropy method. Works seamlessly for the single-system limit. Lower boundary breaks in the 4-phase regime where the density of solid crystals becomes more buoyant than bulk density

---
+++ Fe_melting/src/solve_thermochem_entr.m
@@ -0,0 +1,218 @@
+% planetesimal: solve thermo-chemical equations
+
+if NUM.step>0
+    NUM.step;
+end
+
+% store previous iteration
+Ti    = SOL.T;
+fFeli = CHM.fFel;
+fSili = CHM.fSil;
+
+
+%% update entropy and Temperature
+advn_S  = -advection(MAT.rho.*CHM.xSi.*CHM.fSis.*SOL.sSis ,UsSi,WsSi,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    -  advection(MAT.rho.*CHM.xFe.*CHM.fFes.*SOL.sFes ,UsFe,WsFe,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    -  advection(MAT.rho.*CHM.xSi.*CHM.fSil.*SOL.sSil ,UlSi,WlSi,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    -  advection(MAT.rho.*CHM.xFe.*CHM.fFel.*SOL.sFel ,UlFe,WlFe,NUM.h,NUM.h,NUM.ADVN,'flx');
+% enthalpy on the outside
+qTz    = - (MAT.kT(1:end-1,:)+MAT.kT(2:end,:))./2 .* ddz(SOL.T,NUM.h);                     % heat diffusion z-flux
+qTx    = - (MAT.kT(:,1:end-1)+MAT.kT(:,2:end))./2 .* ddx(SOL.T,NUM.h);                     % heat diffusion x-flux
+diff_T(2:end-1,2:end-1) = (- ddz(qTz(:,2:end-1),NUM.h)  ...                    % heat diffusion
+    - ddx(qTx(2:end-1,:),NUM.h));
+
+
+diss_T = zeros(size(SOL.T));
+diss_T(2:end-1,2:end-1) = EntProd ./SOL.T(2:end-1,2:end-1);
+
+dSdt   = advn_S + diff_T + diss_T; % + MAT.Hr;
+
+if NUM.step>0
+    % update solution
+    SOL.S = So + (NUM.theta.*dSdt + (1-NUM.theta).*dSdto) .* NUM.dt;
+
+    %apply boundaries
+    switch SOL.BCTTop
+        case 'isothermal'
+            SOL.S(1,:)      = MAT.rho(1,:).*(PHY.Cp.*log(SOL.T0./SOL.T0))+PHY.aT./rhoRef.*(SOL.Pt(1,:)-P0) + MAT.Ds(1,:);% + adiabatic + mixture entropy?
+            %             SOL.S(1,:)      = MAT.rho(1,:).*PHY.Cp.*SOL.T0./SOL.T0;
+
+            %             SOL.S(1,:)      =  2*SOL.T0.*(MAT.rho(1,:).*(MAT.Ds(1,:) +PHY.Cp)) - SOL.H(2,:);
+        case 'insulating'
+            SOL.S(1,:)      =  SOL.S(2,:);
+            SOL.H(1,:)      =  SOL.H(2,:);
+        case 'flux'
+
+    end
+    switch SOL.BCTBot
+        case 'isothermal'
+            SOL.S(end,:)      = MAT.rho(end,:).*(PHY.Cp*log(SOL.T1./SOL.T0))+PHY.aT./rhoRef.*(SOL.Pt(end,:)-P0) + MAT.Ds(end,:);
+            %             SOL.S(end,:)      = MAT.rho(end,:).*PHY.Cp*SOL.T1./SOL.T0;
+        case 'insulating'
+            SOL.S(end,:)    =  SOL.S(end-1,:);
+    end
+    switch SOL.BCTSides
+        case 'isothermal'
+            SOL.S(:,[1 end])  = MAT.rho(:,[1 end])*PHY.Cp*log(SOL.T0./SOL.T0);
+            SOL.H(:,[1 end])  =  SOL.T0.*(MAT.rho(:,[1 end]).*(MAT.Ds(:,[1 end]) + PHY.Cp));
+        case 'insulating'
+            SOL.S(:,[1 end])  =  SOL.S(:,[2 end-1]);
+            SOL.H(:,[1 end])  =  SOL.H(:,[2 end-1]);
+    end
+
+end
+
+
+%% update composition
+
+% update system, only one system needs to be solved as SUM_i(X_i) = 1
+% equation 7
+adv_XFe     = advection(MAT.rho.*CHM.xFe.*CHM.fFes,UsFe,WsFe,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    + advection(MAT.rho.*CHM.xFe.*CHM.fFel,UlFe,WlFe,NUM.h,NUM.h,NUM.ADVN,'flx');
+dXdt        = - adv_XFe;
+
+if NUM.step>0 && any(CHM.xFe(:)>0) && any(CHM.xFe(:)<1)
+    % update solution
+    CHM.XFe = XFeo + (NUM.theta.*dXdt + (1-NUM.theta).*dXdto) .* NUM.dt;
+
+    % apply boundaries
+    CHM.XFe([1 end],:) = CHM.XFe([2 end-1],:);  CHM.XFe(:,[1 end]) = CHM.XFe(:,[2 end-1]);
+else
+    CHM.XFe = CHM.xFe.*MAT.rho;
+end
+
+% update fertile chemical components
+% equations 8a, 8b
+advn_CSi  = advection(MAT.rho.*CHM.xSi.*CHM.fSis.*CHM.csSi,UsSi,WsSi,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    + advection(MAT.rho.*CHM.xSi.*CHM.fSil.*CHM.clSi,UlSi,WlSi,NUM.h,NUM.h,NUM.ADVN,'flx');
+
+qCSiz     = - PHY.kC.* (MAT.rho (1:end-1,:)+MAT.rho (2:end,:))/2 ...
+    .* (CHM.xSi (1:end-1,:)+CHM.xSi (2:end,:))/2 ...
+    .* (CHM.fSil(1:end-1,:)+CHM.fSil(2:end,:))/2 ...
+    .* ddz(CHM.cSi,NUM.h);                                 % chemical diffusion z-flux
+qCSix     = - PHY.kC.* (MAT.rho (:,1:end-1)+MAT.rho (:,2:end))/2 ...
+    .* (CHM.xSi (:,1:end-1)+CHM.xSi (:,2:end))/2 ...
+    .* (CHM.fSil(:,1:end-1)+CHM.fSil(:,2:end))/2 ...
+    .* ddx(CHM.cSi,NUM.h);
+diff_CSi(2:end-1,2:end-1) = (- ddz(qCSiz(:,2:end-1),NUM.h) ...             % chemical diffusion
+    - ddx(qCSix(2:end-1,:),NUM.h));
+
+dCSidt    = - advn_CSi + diff_CSi;
+
+advn_CFe  = advection(MAT.rho.*CHM.xFe.*CHM.fFes.*CHM.csFe,UsFe,WsFe,NUM.h,NUM.h,NUM.ADVN,'flx')...
+    + advection(MAT.rho.*CHM.xFe.*CHM.fFel.*CHM.clFe,UlFe,WlFe,NUM.h,NUM.h,NUM.ADVN,'flx');
+
+qCFez     = - PHY.kC.* (MAT.rho (1:end-1,:)+MAT.rho (2:end,:))/2 ...
+    .* (CHM.xFe (1:end-1,:)+CHM.xFe (2:end,:))/2 ...
+    .* (CHM.fFel(1:end-1,:)+CHM.fFel(2:end,:))/2 ...
+    .* ddz(CHM.cFe,NUM.h);                                 % chemical diffusion z-flux
+qCFex     = - PHY.kC.* (MAT.rho (:,1:end-1)+MAT.rho (:,2:end))/2 ...
+    .* (CHM.xFe (:,1:end-1)+CHM.xFe (:,2:end))/2 ...
+    .* (CHM.fFel(:,1:end-1)+CHM.fFel(:,2:end))/2 ...
+    .* ddx(CHM.cFe,NUM.h);                                 % chemical diffusion z-flux
+diff_CFe(2:end-1,2:end-1) = (- ddz(qCFez(:,2:end-1),NUM.h) ...             % chemical diffusion
+    - ddx(qCFex(2:end-1,:),NUM.h));
+
+dCFedt      = - advn_CFe + diff_CFe;
+
+if NUM.step>0
+    % update solution
+    CHM.CSi = CSio + (NUM.theta.*dCSidt + (1-NUM.theta).*dCSidto) .* NUM.dt;
+    CHM.CFe = CFeo + (NUM.theta.*dCFedt + (1-NUM.theta).*dCFedto) .* NUM.dt;
+
+    % apply boundaries
+    CHM.CSi([1 end],:) = CHM.CSi([2 end-1],:);  CHM.CSi(:,[1 end]) = CHM.CSi(:,[2 end-1]);
+    CHM.CFe([1 end],:) = CHM.CFe([2 end-1],:);  CHM.CFe(:,[1 end]) = CHM.CFe(:,[2 end-1]);
+end
+
+if NUM.step>0
+    CHM.XSi = MAT.rho - CHM.XFe;
+    CHM.xFe = CHM.XFe./MAT.rho;
+    CHM.xSi = 1-CHM.xFe;
+    CHM.cSi = CHM.CSi./(CHM.xSi+TINY)./MAT.rho;
+    CHM.cFe = CHM.CFe./(CHM.xFe+TINY)./MAT.rho;
+    SOL.T    = SOL.T0.*exp(SOL.S./MAT.rho./PHY.Cp ...
+             + PHY.aT.*(SOL.Pt - P0)./rhoRef./PHY.Cp...
+             - MAT.Ds./PHY.Cp);
+else
+    SOL.S   = MAT.rho.*(PHY.Cp.*log(SOL.T/SOL.T0) - PHY.aT./rhoRef.*(SOL.Pt-P0) + MAT.Ds);
+    SOL.H   = SOL.T.*(MAT.rho.*(PHY.Cp + MAT.Ds));
+    CHM.XFe = MAT.rho.*CHM.xFe; CHM.XSi = MAT.rho.*CHM.xSi;
+    CHM.XSi = MAT.rho - CHM.XFe;
+    CHM.CFe = MAT.rho.*CHM.cFe.*CHM.xFe;
+    CHM.CSi = MAT.rho.*CHM.cSi.*CHM.xSi;
+end
+
+
+%% update local phase equilibrium
+[fFesq,csFeq,clFeq] = equilibrium_single(SOL.T,CHM.cFe,SOL.Pt,CHM.TFe1,CHM.TFe2,CHM.cphsFe1,CHM.cphsFe2,...
+    CHM.perTFe,CHM.perCsFe,CHM.perClFe,CHM.clap,CHM.PhDgFe,TINY);
+
+[fSisq,csSiq,clSiq] = equilibrium(SOL.T,CHM.cSi,SOL.Pt,CHM.TSi1,CHM.TSi2,CHM.cphsSi1,CHM.cphsSi2,...
+    CHM.perTSi,CHM.perCsSi,CHM.perClSi,CHM.clap,CHM.PhDgSi,TINY);
+
+fFelq = 1-fFesq;
+fSilq = 1-fSisq;
+
+% update phase fractions
+if RUN.diseq
+    CHM.GFe = alpha.*CHM.GFe + (1-alpha).*((fFelq-CHM.fFel).*CHM.XFe./max(4.*NUM.dt,CHM.tau_r));
+
+    advn_FFe = advection(MAT.rho.*CHM.xFe.*CHM.fFel,UlFe,WlFe,NUM.h,NUM.h,NUM.ADVN,'flx');            % get advection term
+
+    dFFedt   = - advn_FFe + CHM.GFe;
+
+    CHM.GSi = alpha.*CHM.GSi + (1-alpha).*((fSilq-CHM.fSil).*CHM.XSi./max(4.*NUM.dt,CHM.tau_r));
+
+    advn_FSi = advection(MAT.rho.*CHM.xSi.*CHM.fSil,UlSi,WlSi,NUM.h,NUM.h,NUM.ADVN,'flx');            % get advection term
+
+    dFSidt   = - advn_FSi + CHM.GSi;                                       % total rate of change
+
+
+
+    if NUM.step>0 % update phase 
+        FFe = rhoo.*xFeo.*fFelo + (NUM.theta.*dFFedt + (1-NUM.theta).*dFFedto).*NUM.dt; FFe = min(MAT.rho-TINY,max(TINY,FFe));
+        FSi = rhoo.*xSio.*fSilo + (NUM.theta.*dFSidt + (1-NUM.theta).*dFSidto).*NUM.dt; FSi = min(MAT.rho-TINY,max(TINY,FSi));
+        CHM.fFel = FFe./(CHM.xFe+TINY)./MAT.rho;
+        CHM.fSil = FSi./(CHM.xSi+TINY)./MAT.rho;  % explicit update of crystal fractionCHM.fSil = min(1-TINY,max(TINY,CHM.fSil));                             % enforce [0,1] limit
+    end
+
+    CHM.fFel([1 end],:) = CHM.fFel([2 end-1],:);                           % apply boundary conditions
+    CHM.fFel(:,[1 end]) = CHM.fFel(:,[2 end-1]);
+
+    CHM.fSil([1 end],:) = CHM.fSil([2 end-1],:);                           % apply boundary conditions
+    CHM.fSil(:,[1 end]) = CHM.fSil(:,[2 end-1]);
+    
+    CHM.fFes = 1-CHM.fFel; CHM.fSis = 1-CHM.fSil;
+
+else
+    % lag equilibrium phase fractions
+    CHM.fFes = alpha.*CHM.fFes + (1-alpha).*fFesq;
+    CHM.fSis = alpha.*CHM.fSis + (1-alpha).*fSisq;
+
+    CHM.fFel = 1-CHM.fFes; CHM.fSil = 1-CHM.fSis;
+
+    CHM.GFe = (MAT.rho.*CHM.fFel-rhoo.*fFelo)./NUM.dt + advection(MAT.rho.*CHM.xFe.*CHM.fFel,UlFe,WlFe,NUM.h,NUM.h,NUM.ADVN,'flx');  % reconstruct iron melting rate
+    CHM.GSi = (MAT.rho.*CHM.fSil-rhoo.*fSilo)./NUM.dt + advection(MAT.rho.*CHM.xSi.*CHM.fSil,UlSi,WlSi,NUM.h,NUM.h,NUM.ADVN,'flx');  % reconstruct silicate melting rate
+end
+
+% update phase compositions
+KcFe = csFeq./clFeq;
+CHM.clFe = CHM.cFe./(CHM.fFel + CHM.fFes.*KcFe);
+CHM.csFe = CHM.cFe./(CHM.fFel./KcFe + CHM.fFes);
+
+KcSi = csSiq./clSiq;
+CHM.clSi = CHM.cSi./(CHM.fSil + CHM.fSis.*KcSi);
+CHM.csSi = CHM.cSi./(CHM.fSil./KcSi + CHM.fSis);
+
+% update entropies
+SOL.sFes = SOL.S./MAT.rho -MAT.Ds;
+SOL.sSis = SOL.S./MAT.rho -MAT.Ds;
+SOL.sFel = SOL.sFes + CHM.dEntrFe;
+SOL.sSil = SOL.sSis + CHM.dEntrSi;
+sumS = MAT.rho.*(CHM.xSi.*CHM.fSis.*SOL.sSis +CHM.xSi.*CHM.fSil.*SOL.sSil + CHM.xFe.*CHM.fFes.*SOL.sFes +CHM.xFe.*CHM.fFel.*SOL.sFel);
+
+% get residual of thermochemical equations from iterative update
+resnorm_TC = norm(SOL.T    - Ti   ,2)./norm(SOL.T   ,2) ...
+    + norm(CHM.fFel - fFeli,2)./norm(CHM.fFel,2) ...
+    + norm(CHM.fSil - fSili,2)./norm(CHM.fSil,2);
--- Fe_melting/src/up2date.m
+++ Fe_melting/src/up2date.m
@@ -4,10 +4,10 @@ tic;  % start clock on update
 
 %% convert weight to volume fraction
 % update T and chemical-dependent density
-MAT.rhoSis = PHY.rhoSis.*(1 - PHY.aTSi.*(SOL.T-CHM.perTSi) - PHY.gCSi.*(CHM.csSi-CHM.cphsSi1));
-MAT.rhoSil = PHY.rhoSil.*(1 - PHY.aTSi.*(SOL.T-CHM.perTSi) - PHY.gCSi.*(CHM.clSi-CHM.cphsSi1));
-MAT.rhoFes = PHY.rhoFes.*(1 - PHY.aTFe.*(SOL.T-CHM.TFe1)   - PHY.gCFe.*(CHM.csFe-CHM.cphsFe1));
-MAT.rhoFel = PHY.rhoFel.*(1 - PHY.aTFe.*(SOL.T-CHM.TFe1)   - PHY.gCFe.*(CHM.clFe-CHM.cphsFe1));
+MAT.rhoSis = PHY.rhoSis.*(1 - PHY.aT.*(SOL.T-CHM.perTSi) - PHY.gCSi.*(CHM.csSi-CHM.cphsSi1));
+MAT.rhoSil = PHY.rhoSil.*(1 - PHY.aT.*(SOL.T-CHM.perTSi) - PHY.gCSi.*(CHM.clSi-CHM.cphsSi1));
+MAT.rhoFes = PHY.rhoFes.*(1 - PHY.aT.*(SOL.T-CHM.TFe1)   - PHY.gCFe.*(CHM.csFe-CHM.cphsFe1));
+MAT.rhoFel = PHY.rhoFel.*(1 - PHY.aT.*(SOL.T-CHM.TFe1)   - PHY.gCFe.*(CHM.clFe-CHM.cphsFe1));
 
 % update mixture density
 MAT.rho    = 1./(CHM.xFe.*CHM.fFes./MAT.rhoFes + CHM.xFe.*CHM.fFel./MAT.rhoFel ...
@@ -27,7 +27,7 @@ etas   = zeros(size(MAT.phiSis)) + PHY.EtaSol0;
 etaSil = PHY.EtaSil0 .* exp(Em./(8.3145.*(SOL.T+273.15))-Em./(8.3145.*((CHM.TFe1+CHM.TFe2)/2+273.15)));
 etaFel = zeros(size(MAT.phiFel)) + PHY.EtaFel0;
 
-% get momentum permission
+% get permission weights
 kv = permute(cat(3,etas,etaSil,etaFel),[3,1,2]);
 Mv = permute(repmat(kv,1,1,1,3),[4,1,2,3])./permute(repmat(kv,1,1,1,3),[1,4,2,3]);
  
@@ -36,26 +36,34 @@ FF = permute(repmat(ff,1,1,1,3),[4,1,2,3]);
 Sf = (FF./BBP).^(1./CCP);  Sf = Sf./sum(Sf,2);
 Xf = sum(AAP.*Sf,2).*FF + (1-sum(AAP.*Sf,2)).*Sf;
 
+% get momentum and volume permissions
 thtv = squeeze(prod(Mv.^Xf,2));
 
 % get momentum flux and transfer coefficients
-Kv =    ff .*kv.*thtv;
-Cv = (1-ff)./[PHY.dx;PHY.dm;PHY.df].^2.*Kv;
+Cv = ((1-ff)./[PHY.dx;1e-16;PHY.df].^2.*kv.*thtv).^-1;
 
 % compose effective viscosity, segregation coefficients
-MAT.Eta  = squeeze(sum(Kv,1));                                             % effective magma viscosity
-MAT.Eta  = (1/etamax + 1./(MAT.Eta + etamin)).^(-1);                       % limit viscosity range
+MAT.Eta  = squeeze(sum(ff.*kv.*thtv,1));                                             % effective magma viscosity
+MAT.Eta  = (1./etamax + 1./(MAT.Eta + etamin)).^(-1);                       % limit viscosity range
+MAT.Eta([1 end],:) = MAT.Eta([2 end-1],:);  
+MAT.Eta(:,[1 end]) = MAT.Eta(:,[2 end-1]);
 MAT.EtaC = (MAT.Eta(1:end-1,1:end-1)+MAT.Eta(2:end,1:end-1) ...            % viscosity in cell corners
          +  MAT.Eta(1:end-1,2:end  )+MAT.Eta(2:end,2:end  ))./4;
 
-Ksgr_x   = max(1e-15,min(1e-6,(MAT.phiSis+MAT.phiFes)./squeeze(Cv(1,:,:))));
-Ksgr_m   = max(1e-15,min(1e-6, MAT.phiSil            ./squeeze(Cv(2,:,:))));
-Ksgr_f   = max(1e-15,min(1e-6, MAT.phiFel            ./squeeze(Cv(3,:,:))));
+% Ksgr_x   = max(1e-15,min(1e-6,(MAT.phiSis+MAT.phiFes)./squeeze(Cv(1,:,:))));
+% Ksgr_m   = max(1e-15,min(1e-6, MAT.phiSil            ./squeeze(Cv(2,:,:))));
+% Ksgr_f   = max(1e-15,min(1e-6, MAT.phiFel            ./squeeze(Cv(3,:,:))));
+%test alternative
+Ksgr_x = squeeze(Cv(1,:,:)) + 1e-18;  Ksgr_x([1 end],:) = Ksgr_x([2 end-1],:);  Ksgr_x(:,[1 end]) = Ksgr_x(:,[2 end-1]);
+Ksgr_m = squeeze(Cv(2,:,:)) + 1e-18;  Ksgr_m([1 end],:) = Ksgr_m([2 end-1],:);  Ksgr_m(:,[1 end]) = Ksgr_m(:,[2 end-1]);
+Ksgr_f = squeeze(Cv(3,:,:)) + 1e-18;  Ksgr_f([1 end],:) = Ksgr_f([2 end-1],:);  Ksgr_f(:,[1 end]) = Ksgr_f(:,[2 end-1]);
+
+
 
 
 %% other bulk thermochemical properties
 % Update pressure
-% SOL.Pt      = rhoRef.*PHY.gzP.*NUM.ZP + SOL.P;
+SOL.Pt      = rhoRef.*PHY.gzP.*NUM.ZP + SOL.P + P0;
 
 % update heat capacities
 % MAT.rhoCp = (MAT.rho.*CHM.xFe.*(CHM.fFes.*PHY.CpFes + CHM.fFel.*PHY.CpFel)...  % mixture sensible heat capacity density
@@ -63,32 +71,21 @@ Ksgr_f   = max(1e-15,min(1e-6, MAT.phiFel            ./squeeze(Cv(3,:,:))));
 % MAT.rhoDs = (MAT.rho.*CHM.xFe.*CHM.fFel.*CHM.dEntrFe...                        % mixture latent heat capacity density
 %           +  MAT.rho.*CHM.xSi.*CHM.fSil.*CHM.dEntrSi);
 MAT.Ds    =  CHM.xFe.*CHM.fFel.*CHM.dEntrFe + CHM.xSi.*CHM.fSil.*CHM.dEntrSi;   % mixture entropy
-MAT.kT    =  CHM.xFe.*PHY.kTFe + CHM.xSi.*PHY.kTSi;                            % magma thermal conductivity
+MAT.kT    =  (CHM.xFe.*PHY.kTFe + CHM.xSi.*PHY.kTSi)./SOL.T;                            % magma thermal conductivity
 
 %% calculate stokes settling velocity
 sds = SOL.BCsides;      % side boundary type 
 % also runge kutta advection time stepping
 kappaseg = 100;
-segSis      = ((MAT.rhoSis(1:end-1,:) + MAT.rhoSis(2:end,:))/2 ...
-            -  (MAT.rho   (1:end-1,:) + MAT.rho   (2:end,:))/2)...
-    .* (Ksgr_x    (1:end-1,:) .*Ksgr_x    (2:end,:)).^0.5  .*  PHY.gz; % silicate particle settling speed
+
+% test alternative
+segSis = (((MAT.rhoSis(1:end-1,:)+MAT.rhoSis(2:end,:))./2-(MAT.rho(1:end-1,:)+MAT.rho(2:end,:))./2).*PHY.gz).*2./(1./Ksgr_x(1:end-1,:)+1./Ksgr_x(2:end,:));
+segFes = (((MAT.rhoFes(1:end-1,:)+MAT.rhoFes(2:end,:))./2-(MAT.rho(1:end-1,:)+MAT.rho(2:end,:))./2).*PHY.gz).*2./(1./Ksgr_x(1:end-1,:)+1./Ksgr_x(2:end,:));
+segSil = (((MAT.rhoSil(1:end-1,:)+MAT.rhoSil(2:end,:))./2-(MAT.rho(1:end-1,:)+MAT.rho(2:end,:))./2).*PHY.gz).*2./(1./Ksgr_m(1:end-1,:)+1./Ksgr_m(2:end,:));
+segFel = (((MAT.rhoFel(1:end-1,:)+MAT.rhoFel(2:end,:))./2-(MAT.rho(1:end-1,:)+MAT.rho(2:end,:))./2).*PHY.gz).*2./(1./Ksgr_f(1:end-1,:)+1./Ksgr_f(2:end,:));
 
 
-segFes      = ((MAT.rhoFes(1:end-1,:) + MAT.rhoFes(2:end,:))/2 ...
-            -  (MAT.rho   (1:end-1,:) + MAT.rho   (2:end,:))/2)...
-    .* (Ksgr_x    (1:end-1,:) .*Ksgr_x    (2:end,:)).^0.5  .*  PHY.gz; % iron particle settling speed
-
-
-segSil      = ((MAT.rhoSil(1:end-1,:) + MAT.rhoSil(2:end,:))/2 ...
-            -  (MAT.rho   (1:end-1,:) + MAT.rho   (2:end,:))/2)...
-    .* (Ksgr_m    (1:end-1,:) .*Ksgr_m    (2:end,:)).^0.5  .*  PHY.gz.*Siseg; % silicate particle settling speed
-
-
-segFel      = ((MAT.rhoFel(1:end-1,:) + MAT.rhoFel(2:end,:))/2 ...
-            -  (MAT.rho   (1:end-1,:) + MAT.rho   (2:end,:))/2)...
-    .* (Ksgr_f    (1:end-1,:) .*Ksgr_f    (2:end,:)).^0.5  .*  PHY.gz; % iron particle settling speed
-
-
+% zero segregation at the boundaries for accumulation
 for nsmooth = 1:nvsmooth
     segSis(2:end-1,2:end-1)   = segSis(2:end-1,2:end-1) + diff(segSis(:,2:end-1),2,1)./8 + diff(segSis(2:end-1,:),2,2)./8;
     segSis([1 end],:) = 0;
@@ -137,6 +134,30 @@ VolSrc = -((MAT.rho-rhoo)/NUM.dt + (Div_rhoV - MAT.rho.*Div_V))./MAT.rho;
 UBG    = mean(mean(VolSrc(2:end-1,2:end-1)))./2 .* (NUM.L/2 - NUM.XU);
 WBG    = mean(mean(VolSrc(2:end-1,2:end-1)))./2 .* (NUM.D/2 - NUM.ZW);
 
+%% Calculate stress and strain rates
+% update strain rates
+DEF.exx(:,2:end-1) = diff(SOL.U,1,2)./NUM.h - Div_V(:,2:end-1)./3;                     % x-normal strain rate
+DEF.exx([1 end],:) = DEF.exx([2 end-1],:);                                         % apply boundary conditions
+DEF.exx(:,[1 end]) = DEF.exx(:,[2 end-1]);
+DEF.ezz(2:end-1,:) = diff(SOL.W,1,1)./NUM.h - Div_V(2:end-1,:)./3;                        % z-normal strain rate
+DEF.ezz([1 end],:) = DEF.ezz([2 end-1],:);                                         % apply boundary conditions
+DEF.ezz(:,[1 end]) = DEF.ezz(:,[2 end-1]);
+DEF.exz            = 1/2.*(diff(SOL.U,1,1)./NUM.h+diff(SOL.W,1,2)./NUM.h);                     % shear strain rate
+
+% update stresses
+DEF.txx = MAT.Eta .* DEF.exx;                                                          % x-normal stress
+DEF.tzz = MAT.Eta .* DEF.ezz;                                                          % z-normal stress
+DEF.txz = MAT.EtaC.* DEF.exz;   
+
+% entropy production/heat dissipation rate
+[grdTx,grdTz] = gradient(SOL.T,NUM.h);
+EntProd = MAT.kT(2:end-1,2:end-1).*(grdTz(2:end-1,2:end-1).^2 + grdTx(2:end-1,2:end-1).^2)...
+        + DEF.exx(2:end-1,2:end-1).*DEF.txx(2:end-1,2:end-1) + DEF.exx(2:end-1,2:end-1).*DEF.txx(2:end-1,2:end-1)...
+        + 2.*(DEF.exz(1:end-1,1:end-1)+DEF.exz(2:end,1:end-1)+DEF.exz(1:end-1,2:end)+DEF.exz(2:end,2:end))./4 ...
+           .*(DEF.txz(1:end-1,1:end-1)+DEF.txz(2:end,1:end-1)+DEF.txz(1:end-1,2:end)+DEF.txz(2:end,2:end))./4 ...
+        +  CHM.xFe(2:end-1,2:end-1).*CHM.fFes(2:end-1,2:end-1).^2./Ksgr_x(2:end-1,2:end-1) .* ((segFes(1:end-1,2:end-1)+segFes(2:end,2:end-1))./2).^2  ...
+        +  CHM.xFe(2:end-1,2:end-1).*CHM.fFel(2:end-1,2:end-1).^2./Ksgr_f(2:end-1,2:end-1) .* ((segFel(1:end-1,2:end-1)+segFel(2:end,2:end-1))./2).^2  ...
+        +  CHM.xSi(2:end-1,2:end-1).*CHM.fSis(2:end-1,2:end-1).^2./Ksgr_x(2:end-1,2:end-1) .* ((segFes(1:end-1,2:end-1)+segFes(2:end,2:end-1))./2).^2 ;
 
 %% update physical time step
 dtadvn =  NUM.h/2   /max(abs([UlSi(:);WlSi(:);UsSi(:);WsSi(:);UlFe(:);WlFe(:);UsFe(:);WsFe(:)])); % stable timestep for advection
